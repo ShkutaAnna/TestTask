e0f442f5ae00f5eaf600377d2c1ea42d3315798d
Ann Shkuta <shkutaanna@gmail.com>
2020-02-20 19:55:31 +0200

    new obj

--- CSharpTest/WorkDayCalculator.cs
+++ CSharpTest/WorkDayCalculator.cs
@@ -6,33 +6,34 @@ namespace CSharpTest
     {
         public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
         {
+            DateTime endDate = new DateTime(startDate.Year, startDate.Month, startDate.Day);
             if (weekEnds != null)
                 for (int i = 0; i < weekEnds.Length; i++)
                 {
-                    if (startDate < weekEnds[i].StartDate)
+                    if (endDate < weekEnds[i].StartDate)
                     {
-                        int span = (int)(weekEnds[i].StartDate - startDate).TotalDays;
+                        int span = (int)(weekEnds[i].StartDate - endDate).TotalDays;
                         if (span >= dayCount)
                         {
-                            return startDate.AddDays(dayCount - 1);
+                            return endDate.AddDays(dayCount - 1);
                         }
                         else
                         {
                             dayCount -= span;
-                            startDate = weekEnds[i].EndDate.AddDays(1);
+                            endDate = weekEnds[i].EndDate.AddDays(1);
                             continue;
                         }
                     }
-                    else if (weekEnds[i].StartDate <= startDate && startDate <= weekEnds[i].EndDate)
+                    else if (weekEnds[i].StartDate <= endDate && endDate <= weekEnds[i].EndDate)
                     {
-                        startDate = weekEnds[i].EndDate.AddDays(1);
+                        endDate = weekEnds[i].EndDate.AddDays(1);
                         continue;
                     }
-                    else if (startDate > weekEnds[i].EndDate)
+                    else if (endDate > weekEnds[i].EndDate)
                         continue;
                 }
             
-            return startDate.AddDays(dayCount - 1);
+            return endDate.AddDays(dayCount - 1);
         }
         
     }
